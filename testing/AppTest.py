7dc75a54bf489ea335d1db3650850b0ae443fd31
af9g19 <af9g19@soton.ac.uk>
2023-03-08 11:26:33 +0000

    Completed app implementation and testing

--- testing/AppTest.py
+++ testing/AppTest.py
@@ -2,16 +2,16 @@ import unittest
 import sys 
 
 sys.path.append("..")
-from abm import App
+
+from abm.App import App
 
 
 class AppTest(unittest.TestCase):
-
     #Functional tests
     def test_setter(self):
         app = App()
         app.set_number(1)
-        self.assertEqual(app.number, 1)
+        self.assertEqual(app.number, 1, 'Number should be one')
 
     def test_add_one(self):
         app = App()
@@ -36,19 +36,6 @@ class AppTest(unittest.TestCase):
         app.set_number(3)
         app.divide_by_three()
         self.assertEqual(app.number, 1)
-    
-    #Error testing
-    def test_set_decimal(self):
-        app = App()
-        self.assertRaises(app.set_number(3.3), TypeError)
-
-    def test_set_string(self):
-        app = App()
-        self.assertEqual(app.set_number('hello'), TypeError)
-
-    def test_set_bool(self):
-        app = App()
-        self.assertEqual(app.set_number(True), TypeError)
 
 if __name__ == '__main__':
     unittest.main()
