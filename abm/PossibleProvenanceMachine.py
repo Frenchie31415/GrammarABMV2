1ad866241fcb1c0c988de1c129d43166f0d7a61b
af9g19 <af9g19@soton.ac.uk>
2023-03-08 21:03:26 +0000

    Writing pp tomorrow

---
+++ abm/PossibleProvenanceMachine.py
@@ -0,0 +1,11 @@
+
+class PossibleProvenanceMachine:
+    def __init__(self):
+        self.g = None
+
+    def calc_graph(self,fro,to):
+        
+        return None
+
+    def get_graph(self):
+        return self.g
--- testing/PossibleProvenanceMachineTest.py
+++ testing/PossibleProvenanceMachineTest.py
@@ -8,26 +8,30 @@ class PossibleProvenanceMachineTest(unittest.TestCase):
     #Functional tests
     def test_one_to_two(self):
         pp = PossibleProvenanceMachine()
-        exp_graph = nx.read_adjlist("testing_graphs/1to2.adj")   
-        actual_graph = pp.get_graph(1,2)
+        exp_graph = nx.read_adjlist("testing_graphs/1to2.adj")  
+        pp.calc_graph(1,2) 
+        actual_graph = pp.get_graph()
         self.assertEqual(exp_graph, actual_graph)
 
     def test_two_to_one(self):
         pp = PossibleProvenanceMachine()
-        exp_graph = nx.read_adjlist("testing_graphs/2to1.adj")   
-        actual_graph = pp.get_graph(2,1)
+        exp_graph = nx.read_adjlist("testing_graphs/2to1.adj")
+        pp.calc_graph(2,1)    
+        actual_graph = pp.get_graph()
         self.assertEqual(exp_graph, actual_graph)
 
     def test_four_to_one(self):
         pp = PossibleProvenanceMachine()
-        exp_graph = nx.read_adjlist("testing_graphs/4to1.adj")   
-        actual_graph = pp.get_graph(4,1)
+        exp_graph = nx.read_adjlist("testing_graphs/4to1.adj")
+        pp.calc_graph(4,1)   
+        actual_graph = pp.get_graph()
         self.assertEqual(exp_graph, actual_graph)
     
     def test_three_to_two(self):
         pp = PossibleProvenanceMachine()
         exp_graph = nx.read_adjlist("testing_graphs/3to2.adj")   
-        actual_graph = pp.get_graph(3,1)
+        pp.calc_graph(3,1)   
+        actual_graph = pp.get_graph()
         self.assertEqual(exp_graph, actual_graph)
 
 if __name__ == '__main__':
